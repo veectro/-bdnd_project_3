91fd8d73a063303eb8e7763dc8c1de6c86da0159
veectro <veectro@gmail.com>
2022-12-20 23:34:23 +0100

    chore: rename ganache shell command

--- 1_start_ganache-cli.sh
+++ 1_start_ganache-cli.sh
@@ -1,1 +1,1 @@
-ganache-cli -m "spirit supply whale amount human item harsh scare congress discover talent hamster"
+ganache -m "spirit supply whale amount human item harsh scare congress discover talent hamster"
