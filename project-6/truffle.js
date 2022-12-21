096f40980faeace213085d7222e37fc2c4ba72d4
veectro <veectro@gmail.com>
2022-12-21 23:32:47 +0100

    chore: add goerli network

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,21 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+      network_id: 5,
+      gas: 4000000,
+      gasPrice: 3900000000
     }
   }
 };
